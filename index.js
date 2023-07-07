e592b29d86b59e14b454a5943337ebad70bd6cf6
edmond <edmond.wanyama@student.moringaschool.com>
2023-07-07 10:09:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-Array.from(document.getElementsByName(input)).forEach((e,i)=>{
-    e.addEventListener('keyup', (el)=>{
-        if(e.value.length > 0) {
-            document.getElementsByClassName('bi')[i].style.transform= "rotate(180deg)";
+// Array.from(document.getElementsByName(input)).forEach((e,i)=>{
+//     e.addEventListener('keyup', (el)=>{
+//         if(e.value.length > 0) {
+//             document.getElementsByClassName('bi')[i].style.transform= "rotate(180deg)";
 
-        } else
-        document.getElementsByClassName('bi')[i].style.transform= "rotate(0deg)";
-    })
-})
+//         } else
+//         document.getElementsByClassName('bi')[i].style.transform= "rotate(0deg)";
+//     })
+// })
 
 let menu_bx = document.getElementById('menu_bx');
 
