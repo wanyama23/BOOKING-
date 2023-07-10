37b199190d9b5c59a728e6a23429500c2dff7f1e
edmond <edmond.wanyama@student.moringaschool.com>
2023-07-10 22:30:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-// Array.from(document.getElementsByName(input)).forEach((e,i)=>{
-//     e.addEventListener('keyup', (el)=>{
-//         if(e.value.length > 0) {
-//             document.getElementsByClassName('bi')[i].style.transform= "rotate(180deg)";
+// // Array.from(document.getElementsByName(input)).forEach((e,i)=>{
+// //     e.addEventListener('keyup', (el)=>{
+// //         if(e.value.length > 0) {
+// //             document.getElementsByClassName('bi')[i].style.transform= "rotate(180deg)";
 
-//         } else
-//         document.getElementsByClassName('bi')[i].style.transform= "rotate(0deg)";
-//     })
-// })
+// //         } else
+// //         document.getElementsByClassName('bi')[i].style.transform= "rotate(0deg)";
+// //     })
+// // })
+
+
+
+
+const url = "https://service-t2zq.onrender.com/"
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const BusPlaceHolder = ()=>{
+        fetch(url)
+        .then(res =>res.json())
+        .then(content =>{
+            const firstBus = content.Bus[0]
+
+            const BusImg = document.getElementById("poster")
+            const BusTitle = document.getElementById("BusTitle")
+            const showingTime = document.getElementById("showtime")
+            const availTicket =document.getElementById("ticketsAvailable")
+            BusImg.src = firstBus.poster
+            BusTitle.innerText = firstBus.title
+            showingTime.innerText =`Showtime: ${firstBus.showtime}`
+            availTicket.innerText =`Tickets Available: (${firstBus.capacity - firstBus.tickets_sold})`
+
 
 let menu_bx = document.getElementById('menu_bx');
 
@@ -121,3 +144,165 @@ function already_sold_tickets(bus){
     return bus.capacity===bus.tickets_sold
     
     }
+
+
+
+
+
+// const url = "https://service-t2zq.onrender.com/"
+
+// document.addEventListener('DOMContentLoaded',()=>{
+
+//     const BusPlaceHolder = ()=>{
+//         fetch(url)
+//         .then(res =>res.json())
+//         .then(content =>{
+//             const firstBus = content.Bus[0]
+
+//             const BusImg = document.getElementById("poster")
+//             const BusTitle = document.getElementById("BusTitle")
+//             // const movieDescr = document.getElementById("movieDescription")
+//             // const runningTime = document.getElementById("runtime")
+//             const showingTime = document.getElementById("showtime")
+//             const availTicket =document.getElementById("ticketsAvailable")
+//             BusImg.src = firstBus.poster
+//             BusTitle.innerText = firstBus.title
+//             // movieDescr.innerText = firstMovie.description
+//             // runningTime.innerText =`Runtime: ${firstMovie.runtime} minutes`
+//             showingTime.innerText =`Showtime: ${firstBus.showtime}`
+//             availTicket.innerText =`Tickets Available: (${firstBus.capacity - firstBus.tickets_sold})`
+
+
+//             const ticketBuy = document.getElementById("buyTicket")
+//             let tickets = Number(firstBus.capacity - firstBus.tickets_sold)
+
+//             ticketBuy.addEventListener('click',()=>{
+
+//                 tickets--
+
+//                 const ticketRemaining = tickets-1
+
+//                 if(tickets <= 0){
+//                     const frstBus = document.getElementById("1")
+//                     frstBus.innerHTML=`${firstBus.title}  <span class="badge bg-danger me-1">SOLD OUT</span>`
+
+//                     availTicket.innerHTML = `Ticketd available:  <span class="badge bg-danger">SOLD OUT</span>`
+//                 }else{
+//                     availTicket.innerText = `Tickets available: (${tickets})`
+//                 }
+//             })
+
+//         })
+
+
+
+
+//     }
+
+
+
+//     const BusDetails = ()=>{
+//         fetch(url)
+//         .then(response=>response.json())
+//         .then(data=>{
+//             const BusData = data.films
+//             console.log(BusData)
+//             for(let i = 0; i < BusData.length; i++){
+//                 let item = BusData[i]
+//                 console.log(item)
+//                 const BusList = document.createElement("li")
+//                 const list = document.getElementById("showingMovie")
+
+//                 BusList.classList.add("list-group-item", "border", "border-info", "sinema")
+
+//                 BusList.setAttribute('id',`${item.id}`)
+            
+//                 BusList.innerText = item.title
+//                 console.log(item.title)
+
+
+//                 list.appendChild(BusList)
+
+//                 BusList.addEventListener('click',()=>{
+//                     const BusImage = document.getElementById("poster")
+//                     const BusTitle = document.getElementById("BusTitle")
+//                     // const runTime = document.getElementById("runtime")
+//                     const showTime = document.getElementById("showtime")
+//                     const availTickets =document.getElementById("ticketsAvailable")
+
+
+//                     BusImage.src = item.poster
+//                     BusTitle.innerText = item.title
+//                     // runTime.innerHTML =`Runtime:<span>${item.runtime}</span>`
+//                     showTime.innerText =`Departure ${item.showtime}`
+//                     availTickets.innerText =`Tickets available: (${item.capacity - item.tickets_sold})`
+
+//                     const ticketsBuy = document.getElementById("buyTicket")
+//                     let ticket = Number(item.capacity - item.tickets_sold)
+
+//                     ticketsBuy.addEventListener('click',()=>{
+
+//                         const ticketRemain = ticket-1
+//                         ticket --
+//                         if(ticket <= 0){
+//                             BusList.innerHTML =`${item.title} <span class="badge bg-danger">SOLD OUT</span>`
+
+//                             availTickets.innerHTML = `Tickets available: <span class="badge bg-danger">SOLD OUT</span>`
+
+//                         }else{
+
+//                             availTickets.innerText = `Tickets available: (${ticket})`
+//                         }
+//                         availTickets.innerText = `Tickets available: ${ticket}`
+//                         while(ticket> -1){
+//                             availTickets.innerText = `Tickets available: ${ticket}`
+//                             if(ticket === 0){
+//                                 return availTickets.innerText = "SOLD OUT"
+//                             }
+
+//                         }
+
+
+
+
+//                         if(ticket === 0){
+//                             return availTickets.innerText = "SOLD OUT"
+//                         }
+
+//                         for(let i = ticket; i > -1 ; i-=1 ){
+
+//                             const ticketRemain = i
+//                             availTickets.innerText = `Tickets available: ${ticketRemain}`
+//                             if(ticketRemain === 0){
+//                                 availTickets.innerText = "SOLD OUT"
+//                             }
+
+
+//                         }
+
+
+//                     })
+
+
+
+//                 })
+
+
+//             }
+
+
+
+
+//         })
+
+//     }
+
+
+//     movieDetails()
+//     moviePlaceHolder()
+
+// })
+
+
+// BusDetails()
+// BusPlaceHolder()
